b5ace01a9574db233fb4aef2463880289c9764de
jkKania <jacek.kafel.kania@gmail.com>
2020-12-18 16:26:01 +0100

    Repo upload

---
+++ GeneratorBitMap/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace GeneratorBitMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button bttnSave;
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            bttnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bttnSave
+            // 
+            bttnSave.Location = new System.Drawing.Point(143, 200);
+            bttnSave.Name = "bttnSave";
+            bttnSave.Size = new System.Drawing.Size(75, 25);
+            bttnSave.TabIndex = 1;
+            bttnSave.Text = "Save";
+            bttnSave.UseVisualStyleBackColor = true;
+            bttnSave.Click += new System.EventHandler(this.bttnSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 200);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "New Number";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(143, 41);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(66, 20);
+            this.txtNumber.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 299);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(bttnSave);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber;
+    }
+}
+
---
+++ GeneratorBitMap/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeneratorBitMap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private Point? _Previous = null;
+        private Pen _Pen = new Pen(Color.Black, 9);
+
+        public static Bitmap Resize(Bitmap imgPhoto, Size objSize, ImageFormat enuType)
+        {
+            int sourceX = 0;
+            int sourceY = 0;
+
+            int destX = 0;
+            int destY = 0;
+            int destWidth = objSize.Width;
+            int destHeight = objSize.Height;
+
+            Bitmap bmPhoto;
+
+            bmPhoto = new Bitmap(destWidth, destHeight, PixelFormat.Format24bppRgb);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            grPhoto.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, destWidth, destHeight),
+                new Rectangle(sourceX, sourceY, imgPhoto.Width, imgPhoto.Height),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            return bmPhoto;
+        }
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _Previous = new Point(e.X, e.Y);
+            pictureBox1_MouseMove(sender, e);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_Previous != null)
+            {
+                if (pictureBox1.Image == null)
+                {
+                    Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.Clear(Color.White);
+                    }
+                    pictureBox1.Image = bmp;
+                }
+                using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+                {
+                    g.SmoothingMode = SmoothingMode.AntiAlias;
+                    g.DrawLine(_Pen, _Previous.Value.X, _Previous.Value.Y, e.X, e.Y);
+                }
+                pictureBox1.Invalidate();
+                _Previous = new Point(e.X, e.Y);
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _Previous = null;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bttnSave_Click(object sender, EventArgs e)
+        {
+            string path = @"learningBase.data";
+            StringBuilder sb = new StringBuilder();
+
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+           
+           
+            Bitmap objImage = new Bitmap(bmp);
+            Size objNewSize = new Size(20, 10);
+            Bitmap objNewImage = Resize(objImage, objNewSize, ImageFormat.MemoryBmp);
+
+
+
+            for (int i = 0; i < objNewImage.Height; i++)
+            {
+                for (int j = 0; j < objNewImage.Width; j++)
+                {
+                    if (objNewImage.GetPixel(j, i) == Color.FromArgb(0, 0, 0))
+                    {
+                        sb.Append("1 ");
+                    }
+                    else
+                    {
+                        sb.Append("0 ");
+                    }
+                    
+                }
+            }
+            File.AppendAllText(path, sb + Environment.NewLine);
+
+            int jakaToLiczba = Int32.Parse(txtNumber.Text);
+            int[] jakaToLiczbaBinary;
+            jakaToLiczbaBinary = new int[10];
+
+            
+
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i != jakaToLiczba)
+                    {
+                        jakaToLiczbaBinary[i] = 0;
+                    }
+                    else
+                    {
+                        jakaToLiczbaBinary[i] = 1;
+                    }
+                    sw.Write(jakaToLiczbaBinary[i] + " ");
+                }
+                sw.WriteLine();
+            }
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+            }
+            pictureBox1.Image = bmp;
+        }
+    }
+}
